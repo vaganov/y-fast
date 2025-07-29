f39c3d5787e97d902921c70c6b2c01de64ea0e65
Nikita Vaganov <nikita.e.vaganov@gmail.com>
2025-07-30 00:33:24 +0200

    Jam balance_factor into child pointers

--- include/yfast/internal/avl.h
+++ include/yfast/internal/avl.h
@@ -7,9 +7,44 @@ namespace yfast::internal {
 
 template <typename Key, typename T>
 struct AVLNodeBase: public BSTNodeBase<Key, T> {
-    int balance_factor;
+    using typename BSTNodeBase<Key, T>::Child;
+
+    using BSTNodeBase<Key, T>::_left;
+    using BSTNodeBase<Key, T>::_right;
 
     explicit AVLNodeBase(const Key& key): BSTNodeBase<Key, T>(key) {}
+
+    [[nodiscard]] bool is_left_heavy() const { return Child(_left).get_bit(0); }
+    [[nodiscard]] bool is_right_heavy() const { return Child(_right).get_bit(0); }
+    [[nodiscard]] bool is_balanced() const { return !is_left_heavy() && !is_right_heavy(); }
+
+    void set_left_heavy() {
+        Child left_child = _left;
+        left_child.set_bit(0);
+        _left = left_child.value;
+
+        Child right_child = _right;
+        right_child.clear_bit(0);
+        _right = right_child.value;
+    }
+    void set_right_heavy() {
+        Child left_child = _left;
+        left_child.clear_bit(0);
+        _left = left_child.value;
+
+        Child right_child = _right;
+        right_child.set_bit(0);
+        _right = right_child.value;
+    }
+    void set_balanced() {
+        Child left_child = _left;
+        left_child.clear_bit(0);
+        _left = left_child.value;
+
+        Child right_child = _right;
+        right_child.clear_bit(0);
+        _right = right_child.value;
+    }
 };
 
 }
--- include/yfast/internal/bst.h
+++ include/yfast/internal/bst.h
@@ -3,17 +3,34 @@
 
 #include <cstddef>
 
+#include <yfast/internal/aligned.h>
+
 namespace yfast::internal {
 
 template <typename _Key, typename T>
 struct BSTNodeBase {
     typedef _Key Key;
+    typedef aligned_ptr<1, T> Child;
 
     const Key key;
     T* parent;
-    T* left;
-    T* right;
+    std::uintptr_t _left;
+    std::uintptr_t _right;
     std::size_t size;
+
+    T* left() const { return Child(_left).get_ptr(); }
+    T* right() const { return Child(_right).get_ptr(); }
+
+    void set_left(T* node) {
+        Child child = _left;
+        child.set_ptr(node);
+        _left = child.value;
+    }
+    void set_right(T* node) {
+        Child child = _right;
+        child.set_ptr(node);
+        _right = child.value;
+    }
 };
 
 }
