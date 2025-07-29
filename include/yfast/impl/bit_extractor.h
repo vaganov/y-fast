fc81790c54153ed29d8d69795eb47c4cc298e6fd
Nikita Vaganov <nikita.e.vaganov@gmail.com>
2025-07-29 23:43:58 +0200

    Try to refine bx for byte array with span view Would not work out right away since keys get deleted Let's keep the draft anyway

--- include/yfast/impl/bit_extractor.h
+++ include/yfast/impl/bit_extractor.h
@@ -2,11 +2,10 @@
 #define _YFAST_IMPL_BIT_EXTRACTOR_H
 
 #include <cstddef>
+#include <span>
 #include <string>
 #include <type_traits>
 #include <vector>
-
-#include <yfast/internal/hash.h>
 
 namespace yfast::impl {
 
@@ -25,7 +24,23 @@ public:
 template <>
 class BitExtractor<std::vector<std::byte>> {
 public:
-    typedef std::vector<std::byte> ShiftResult;
+    class ShiftResult {
+        const std::vector<std::byte> _data;
+
+    public:
+        const std::span<const std::byte> view;
+
+        ShiftResult() = default;
+        explicit ShiftResult(const std::vector<std::byte>& data): _data(data), view(_data.begin(), _data.size()) {}
+        explicit ShiftResult(std::vector<std::byte>&& data): _data(std::move(data)), view(_data) {}
+        explicit ShiftResult(const std::span<const std::byte>& span): view(span) {}
+        ShiftResult(const ShiftResult& other): _data(other._data), view(_data.empty() ? other.view : std::span(_data)) {}
+        ShiftResult(ShiftResult&& other) noexcept: _data(other._data), view(_data.empty() ? other.view : std::span(_data)) {}
+
+        bool operator == (const ShiftResult& other) const {
+            return std::ranges::equal(view, other.view);
+        }
+    };
 
     static bool extract_bit(const std::byte* data, std::size_t size, unsigned int n) {
         const auto s = n / 8;
@@ -43,29 +58,28 @@ public:
 
     // [l | r] [l | r] [l | r]
     // [l] [r | l] [r | l]
-    static std::vector<std::byte> shift(const std::byte* data, std::size_t size, unsigned int n) {
+    static ShiftResult shift(const std::byte* data, std::size_t size, unsigned int n) {
         const auto s = n / 8;
         if (size <= s) {
             return {};
         }
 
         const auto shifted_size = size - s;
-        std::vector<std::byte> shifted(shifted_size);
         const auto r = n % 8;
         if (r == 0) {
-            std::ranges::copy(data, data + shifted_size, shifted.begin());
-            return shifted;
+            return ShiftResult(std::span(data, shifted_size));
         }
 
         const auto l = 8 - r;
+        std::vector<std::byte> shifted(shifted_size);
         std::transform(data, data + shifted_size, shifted.begin(), [r] (std::byte b) { return b >> r; });
         for (int i = 1; i < shifted_size; ++i) {
             shifted[i] |= data[i - 1] << l;
         }
-        return shifted;
+        return ShiftResult(std::move(shifted));
     }
 
-    static std::vector<std::byte> shift(const std::vector<std::byte>& key, unsigned int n) {
+    static ShiftResult shift(const std::vector<std::byte>& key, unsigned int n) {
         return shift(key.data(), key.size(), n);
     }
 };
@@ -73,16 +87,27 @@ public:
 template <>
 class BitExtractor<std::string> {
 public:
-    typedef std::vector<std::byte> ShiftResult;
+    typedef BitExtractor<std::vector<std::byte>>::ShiftResult ShiftResult;
 
     static bool extract_bit(const std::string& key, unsigned int n) {
         const auto data = reinterpret_cast<const std::byte*>(key.data());
         return BitExtractor<std::vector<std::byte>>::extract_bit(data, key.size(), n);
     }
 
-    static std::vector<std::byte> shift(const std::string& key, unsigned int n) {
+    static ShiftResult shift(const std::string& key, unsigned int n) {
         const auto data = reinterpret_cast<const std::byte*>(key.data());
         return BitExtractor<std::vector<std::byte>>::shift(data, key.size(), n);
+    }
+};
+
+}
+
+namespace std {
+
+template <>
+struct hash<yfast::impl::BitExtractor<vector<byte>>::ShiftResult> {
+    size_t operator () (const yfast::impl::BitExtractor<vector<byte>>::ShiftResult& shift_result) const noexcept {
+        return hash<string_view>{}({ reinterpret_cast<const char*>(shift_result.view.data()), shift_result.view.size() });
     }
 };
 
