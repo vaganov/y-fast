a929d81ed15155a14c54e8cbd4aa13664888d49d
Nikita Vaganov <nikita.e.vaganov@gmail.com>
2025-08-31 23:10:37 +0200

    Regenerate docs

--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['insert_0',['insert',['../classyfast_1_1fastmap.html#a1921d075ed80a36cd111b617f1bb5b62',1,'yfast::fastmap::insert()'],['../classyfast_1_1impl_1_1_a_v_l.html#a89eef6d0b1572fd48d0dcabe4ae0cdb6',1,'yfast::impl::AVL::insert()'],['../classyfast_1_1impl_1_1_b_s_t.html#a9fa6ebab9d0223ff5adce68c57e080ef',1,'yfast::impl::BST::insert()'],['../classyfast_1_1impl_1_1_x_fast_trie.html#a7f85c2b53684352fd207fbd46220f5b9',1,'yfast::impl::XFastTrie::insert()'],['../classyfast_1_1impl_1_1_y_fast_trie.html#a5358101967616a2413d0888d1a004986',1,'yfast::impl::YFastTrie::insert()']]]
+  ['insert_0',['insert',['../classyfast_1_1fastmap.html#a1921d075ed80a36cd111b617f1bb5b62',1,'yfast::fastmap::insert()'],['../classyfast_1_1impl_1_1_a_v_l.html#a89eef6d0b1572fd48d0dcabe4ae0cdb6',1,'yfast::impl::AVL::insert()'],['../classyfast_1_1impl_1_1_b_s_t.html#a9fa6ebab9d0223ff5adce68c57e080ef',1,'yfast::impl::BST::insert()'],['../classyfast_1_1impl_1_1_x_fast_trie.html#a35bb6f95fe68a6c9e7fa90d4c5962eca',1,'yfast::impl::XFastTrie::insert()'],['../classyfast_1_1impl_1_1_y_fast_trie.html#a5358101967616a2413d0888d1a004986',1,'yfast::impl::YFastTrie::insert()']]]
 ];
